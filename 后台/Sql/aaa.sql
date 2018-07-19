84556896389d768b54ccacba43df1969adbc17bf
朱格利斯 <teersky@sina.com>
2018-07-20 01:16:13 +0800

    新加数据库

---
+++ 后台/Sql/aaa.sql
@@ -0,0 +1,38 @@
+/*
+SQLyog 企业版 - MySQL GUI v8.14 
+MySQL - 5.1.32-community : Database - test
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`test` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `test`;
+
+/*Table structure for table `statetable` */
+
+DROP TABLE IF EXISTS `statetable`;
+
+CREATE TABLE `statetable` (
+  `uId` int(20) NOT NULL,
+  `username` varchar(24) NOT NULL,
+  `keyId` varchar(48) DEFAULT NULL,
+  `timeDate` int(48) DEFAULT NULL,
+  PRIMARY KEY (`uId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `statetable` */
+
+insert  into `statetable`(`uId`,`username`,`keyId`,`timeDate`) values (1,'teersky','68E109F0F40CA72A15E05CC22786F8E6',2147483647);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
