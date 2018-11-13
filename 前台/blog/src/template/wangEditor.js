94529022c92a1f2d32f50d6e70528bda05d76742
teersky <teersky@sina.com>
2018-11-13 17:51:00 +0800

    新加编辑器部分

--- 前台/blog/src/template/wangEditor.js
+++ 前台/blog/src/template/wangEditor.js
@@ -1,8 +1,32 @@
 import React, { Component } from 'react';
 import { Input,  Divider, Button  } from 'antd'
+import Mditor from 'mditor/js/mditor.min.js'
 import './style.css'
+import 'mditor/css/mditor.min.css'
 
 export default class blogEditor extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            selectTips: ""
+        }
+    }
+    SelectTips__artical(items){
+        console.log(items);
+        this.setState({
+            selectTips: items
+        });
+    }
+    setSelectTips(value){
+        console.log(value);
+        this.setState({
+            selectTips: value
+        });
+    }
+    componentDidMount(){
+        var ele = this.refs.md_editor;
+        var mditor =  Mditor.fromTextarea(ele);
+      }
     render() {
         const artical__tips = this.props.artical__tips;
         return (
@@ -12,15 +36,15 @@ export default class blogEditor extends Component {
                     <span>标题：</span>
                     <Input placeholder="请输入文章标题" />
                 </p>
-                <section>
-                    <div className="Editor__Header">
+                <section className="Editor__Header">
+                    <div class="Editor-Header__type">
                         <span>分类：</span>
-                        <div>
-                            <Input placeholder="请输入文章分类" />
+                        <div class="Editor-Header__typeInp">
+                            <Input placeholder="请输入文章分类" id="setSelectTipsInput" onChange ={(e) => {this.setSelectTips(e.target.value)}} value={ this.state.selectTips }/>
                             <p className="Editor__Tips">
                                 {
                                     artical__tips.map((items) => {
-                                        return <Button size="small">{items}</Button>
+                                        return <Button size="small" onClick = {() => { this.SelectTips__artical(items) }}>{items}</Button>
                                     })
                                 }
                             </p>
@@ -28,6 +52,9 @@ export default class blogEditor extends Component {
                     </div>
                 </section>
                 <Divider>正文</Divider>
+                <div>
+                    <textarea id="md_editor" ref="md_editor"></textarea>
+                </div>
            </div>
 
         )
