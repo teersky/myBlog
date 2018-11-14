dc9f622da9910c5f9576082b667c2a5e475a1178
teersky <teersky@sina.com>
2018-11-14 18:11:14 +0800

    新加mditor编辑器项目

--- 前台/blog/src/template/wangEditor.js
+++ 前台/blog/src/template/wangEditor.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { Input,  Divider, Button  } from 'antd'
-import Mditor from 'mditor/js/mditor.min.js'
+/* import Mditor from 'mditor' */
 import './style.css'
-import 'mditor/css/mditor.min.css'
+import Mditor from 'mditor'
+import 'mditor/dist/css/mditor.min.css'
 
 export default class blogEditor extends Component {
     constructor(props){
@@ -25,7 +26,11 @@ export default class blogEditor extends Component {
     }
     componentDidMount(){
         var ele = this.refs.md_editor;
-        var mditor =  Mditor.fromTextarea(ele);
+        
+        //var mditor =  Mditor.fromTextarea(ele);
+       // var parser = new Mditor.Parser();
+        console.log(Mditor);
+        //var html = parser.parse("** Hello mditor! **");
       }
     render() {
         const artical__tips = this.props.artical__tips;
@@ -37,13 +42,13 @@ export default class blogEditor extends Component {
                     <Input placeholder="请输入文章标题" />
                 </p>
                 <section className="Editor__Header">
-                    <div class="Editor-Header__type">
+                    <div className="Editor-Header__type">
                         <span>分类：</span>
-                        <div class="Editor-Header__typeInp">
+                        <div className="Editor-Header__typeInp">
                             <Input placeholder="请输入文章分类" id="setSelectTipsInput" onChange ={(e) => {this.setSelectTips(e.target.value)}} value={ this.state.selectTips }/>
                             <p className="Editor__Tips">
                                 {
-                                    artical__tips.map((items) => {
+                                    artical__tips.map((items, key) => {
                                         return <Button size="small" onClick = {() => { this.SelectTips__artical(items) }}>{items}</Button>
                                     })
                                 }
