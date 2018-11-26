a39a88de637aa7f51083c36ea0cef5e4e6c4f818
teersky <teersky@sina.com>
2018-11-26 16:11:40 +0800

    新加markdown解析成html的动能

--- 前台/blog/src/template/mditorShow.js
+++ 前台/blog/src/template/mditorShow.js
@@ -3,39 +3,28 @@ import { Input,  Divider, Button, Modal } from 'antd'
 import './style.css'
 
 
-import SimpleMDE from 'simplemde'
-import marked from 'marked'
-import highlight from 'highlight.js'
-import 'simplemde/dist/simplemde.min.css'
+import Marked from "marked"
+import PopupMask from "./PopupMask.jsx"
 
 export default class blogEditor extends Component {
     constructor(props){
         super(props);
         this.state = {
-            artical__tit: props.articalTit,
-            artical__txt: props.articalTXT,
-            artical__ID: props.articalID,
-            artical__tip: props.articalTip,
-            isBeready: 1
         }
     }
-    componentDidUpdate(){
-        let self = this;
-        if(self.props.articalTit != "" & this.state.isBeready){
-            this.setState({
-                artical__tit: self.props.articalTit,
-                artical__txt: self.props.articalTXT,
-                artical__ID: self.props.articalID,
-                artical__tip: self.props.articalTip,
-                isBeready: 0
-            })
-        }
-    }
+
 
     render() {
+        let articalDetail = Marked(this.props.articalTXT);
+        let artical__tit = this.props.articalTit;
+        let artical__txt = this.props.articalTXT;
+        let artical__ID = this.props.articalID;
+        let artical__tip = this.props.articalTip;
         return (
-            <div>文章预览</div>
-
+            <section>
+                <div>文章预览</div>
+                <PopupMask><div>真香警告</div></PopupMask>
+            </section>
         )
     }
 }
